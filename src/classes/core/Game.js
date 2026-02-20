4b85c50962ce493ab96779d34a53e219f2a99b9a
larsbeckmusic <larsbeckmusic@gmail.com>
2026-02-20 12:49:14 +0000

    Remove unused FullscreenButton import and related code from Game class

--- src/classes/core/Game.js
+++ src/classes/core/Game.js
@@ -1,7 +1,6 @@
 import Background from "@/classes/rendering/Background.js"; // Background renderer
 import Camera from "@/classes/rendering/Camera.js"; // Camera system
 import Keyboard from "./Keyboard.js"; // Input handler
-import FullscreenButton from "./FullscreenButton.js";
 import World from "./World.js"; // Game world
 
 export default class Game {
@@ -16,9 +15,6 @@ export default class Game {
     this.camera = new Camera(); // Create camera
 
     this.keyboard = new Keyboard(); // Create keyboard
-
-    // Fullscreen Button
-    // this.fullscreenButton = new FullscreenButton(canvas);
 
     this.world = new World({
       canvas: this.canvas, // Pass canvas
