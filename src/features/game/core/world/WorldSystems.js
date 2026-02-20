0235e4d2cfe77ca89c002f3e025d3d0f9b02eeaf
larsbeckmusic <larsbeckmusic@gmail.com>
2026-02-20 13:17:19 +0000

    Remove unused file

---
+++ src/features/game/core/world/WorldSystems.js
@@ -0,0 +1,11 @@
+import MovementSystem from "@/features/game/systems/MovementSystem.js";
+import ThrowSystem from "@/features/game/systems/ThrowSystem.js";
+import CollisionSystem from "@/features/game/systems/CollisionSystem.js";
+
+export function createWorldSystems(world) {
+  return [
+    new MovementSystem(world),
+    new ThrowSystem(world),
+    new CollisionSystem(world),
+  ];
+}
