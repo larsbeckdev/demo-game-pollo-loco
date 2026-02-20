5b025b3e1e5128656b3f9d4c13e34fb9f1c7faf9
larsbeckmusic <larsbeckmusic@gmail.com>
2026-02-20 13:23:07 +0000

    Move CollisionSystem to a new directory and update its import path

--- src/features/game/core/world/WorldSystems.js
+++ src/features/game/core/world/WorldSystems.js
@@ -1,6 +1,6 @@
 import MovementSystem from "@/features/game/systems/movement/MovementSystem.js";
 import ThrowSystem from "@/features/game/systems/ThrowSystem.js";
-import CollisionSystem from "@/features/game/systems/CollisionSystem.js";
+import CollisionSystem from "@/features/game/systems/collision/CollisionSystem.js";
 
 export function createWorldSystems(world) {
   return [
