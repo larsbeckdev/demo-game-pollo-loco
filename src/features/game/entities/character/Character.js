5eed2627bdf05ffbee9b2c7671eb5674a1c4fe01
larsbeckmusic <larsbeckmusic@gmail.com>
2026-02-20 13:59:07 +0000

    Add compatibility layer with legacy property names for Character class

--- src/features/game/entities/character/Character.js
+++ src/features/game/entities/character/Character.js
@@ -83,4 +83,100 @@ export default class Character {
   draw(canvasContext, cameraOffsetX = 0) {
     drawCharacter(this, canvasContext, cameraOffsetX);
   }
+
+  // ------------------------------------------------------------
+  // Compatibility layer: keep old property names working
+  // so existing World/Systems code still draws and moves correctly
+  // ------------------------------------------------------------
+
+  get x() {
+    return this.positionX;
+  }
+  set x(value) {
+    this.positionX = value;
+  }
+
+  get y() {
+    return this.positionY;
+  }
+  set y(value) {
+    this.positionY = value;
+  }
+
+  get w() {
+    return this.width;
+  }
+  set w(value) {
+    this.width = value;
+  }
+
+  get h() {
+    return this.height;
+  }
+  set h(value) {
+    this.height = value;
+  }
+
+  get vx() {
+    return this.horizontalVelocity;
+  }
+  set vx(value) {
+    this.horizontalVelocity = value;
+  }
+
+  get vy() {
+    return this.verticalVelocity;
+  }
+  set vy(value) {
+    this.verticalVelocity = value;
+  }
+
+  get facing() {
+    return this.facingDirection;
+  }
+  set facing(value) {
+    this.facingDirection = value;
+  }
+
+  get onGround() {
+    return this.isOnGround;
+  }
+  set onGround(value) {
+    this.isOnGround = value;
+  }
+
+  get dead() {
+    return this.isDead;
+  }
+  set dead(value) {
+    this.isDead = value;
+  }
+
+  get hurtActive() {
+    return this.isHurt;
+  }
+  set hurtActive(value) {
+    this.isHurt = value;
+  }
+
+  get anims() {
+    return this.animations;
+  }
+  set anims(value) {
+    this.animations = value;
+  }
+
+  get currentAnimKey() {
+    return this.currentAnimationKey;
+  }
+  set currentAnimKey(value) {
+    this.currentAnimationKey = value;
+  }
+
+  get groundY() {
+    return this.groundLevel;
+  }
+  set groundY(value) {
+    this.groundLevel = value;
+  }
 }
