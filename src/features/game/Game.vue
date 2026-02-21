12a73b98b14b27b157cc28efb1eda6391f34ce35
larsbeckmusic <larsbeckmusic@gmail.com>
2026-02-21 16:36:49 +0000

    Update button type from default to primary in the screen-actions overlay

--- src/features/game/Game.vue
+++ src/features/game/Game.vue
@@ -28,7 +28,7 @@
 
         <!-- ✅ Button-Layer über dem Bild -->
         <div class="screen-actions" @click.stop>
-          <n-button type="default" size="large" @click="onOverlayClick">
+          <n-button type="primary" size="large" @click="onOverlayClick">
             {{ screen === "intro" ? "Start" : "Nochmal" }}
           </n-button>
         </div>
