7aa02b1540194319a1be6fc5aea0f9531bd135bc
larsbeckmusic <larsbeckmusic@gmail.com>
2026-02-23 14:07:28 +0000

    Comment out debug logging in the game render method

--- src/features/game/core/Game.js
+++ src/features/game/core/Game.js
@@ -168,11 +168,11 @@ export default class Game {
 
     this.gameWorld.draw(ctx);
 
-    if (this.debug.enabled && this.debug.frameCount % 120 === 0) {
-      console.log("%c[Game Render OK]", "color:purple;", {
-        cameraX: this.cameraSystem.x,
-        canvasWidth: canvas.width,
-      });
-    }
+    // if (this.debug.enabled && this.debug.frameCount % 120 === 0) {
+    //   console.log("%c[Game Render OK]", "color:purple;", {
+    //     cameraX: this.cameraSystem.x,
+    //     canvasWidth: canvas.width,
+    //   });
+    // }
   }
 }
